8d8ce8d39833ff3039b394c2f695d64cb1c11d64
Tartofraise <ryan-68@live.fr>
2020-04-03 23:55:01 +0200

    Create dump.php

---
+++ dump.php
@@ -0,0 +1,63 @@
+<?php
+
+// Database configuration
+$host = "localhost";
+$username = "user";
+$password = "pass";
+$database_name = "database";
+
+// Get connection object and set the charset
+$conn = mysqli_connect($host, $username, $password, $database_name);
+$conn->set_charset("utf8");
+
+
+// Get All Table Names From the Database
+$tables = array();
+$sql = "SHOW TABLES";
+$result = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_row($result)) {
+    $tables[] = $row[0];
+}
+
+$sqlScript = "";
+foreach ($tables as $table) {
+    
+    // Prepare SQLscript for creating table structure
+    $query = "SHOW CREATE TABLE $table";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_row($result);
+    
+    $sqlScript .= "\n\n" . $row[1] . ";\n\n";
+    
+    
+    $query = "SELECT * FROM $table";
+    $result = mysqli_query($conn, $query);
+    
+    $columnCount = mysqli_num_fields($result);
+    
+    // Prepare SQLscript for dumping data for each table
+    for ($i = 0; $i < $columnCount; $i ++) {
+        while ($row = mysqli_fetch_row($result)) {
+            $sqlScript .= "INSERT INTO $table VALUES(";
+            for ($j = 0; $j < $columnCount; $j ++) {
+                $row[$j] = $row[$j];
+                
+                if (isset($row[$j])) {
+                    $sqlScript .= '"' . $row[$j] . '"';
+                } else {
+                    $sqlScript .= '""';
+                }
+                if ($j < ($columnCount - 1)) {
+                    $sqlScript .= ',';
+                }
+            }
+            $sqlScript .= ");\n";
+        }
+    }
+    
+    $sqlScript .= "\n"; 
+}
+
+print $sqlScript; // You can also write to a file
+?>
